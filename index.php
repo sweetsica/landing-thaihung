eddfd5f4c85f4988c55d96553a9c5dc91240532e
Snow Deep <barondeepsnow@gmail.com>
2024-04-15 14:24:19 +0700

    fix mail

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
+<php
+  if(isset($_POST['submit'])){
+      $to = "baonn@doppelherz.vn"; // this is your Email address
+      $from = "baonn@sweetsica.com"; // this is the sender's Email address
+      $file = ($_POST['file'] ? $_POST['file'] : "");
+      $first_name = $_POST['first_name'];
+      $last_name = $_POST['last_name'];
+      $subject = "Lời nhắn từ catalogue Thái Hưng!";
+      $subject2 = "Copy lời nhắn từ catalogue Thái Hưng!";
+      $message = "- Họ tên: ". $first_name . " " . $last_name . "\n\n" . "- Email: " $_POST['email'] . "\n\n" ."- Lời nhắn:" . "\n\n" . $_POST['message'] . "\n\n" ."- Link file đính kèm: " . $_POST['file'] ;
+      $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+      $headers = "From:" . $from;
+      $headers2 = "From:" . $to;
+      mail($to,$subject,$message,$headers);
+      mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+      echo "Gửi tin nhắn thành công! Cảm ơn " . $first_name . ", chúng tôi sẽ liên hệ bạn trong thời gian sớm nhất.";
+      // You can also use header('Location: thank_you.php'); to redirect to another page.
+      }
+ ?>
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -529,7 +549,8 @@
                 </div>
                 <div class="content__right">
                   <div class="content__subtitle">We always welcome you - <span>potential partners</span>. Please leave your information and we will have a discussion soon.</div>
-                  <form class="content__contacts contacts__form js-contact-form" method="post" action="">
+                  <!-- <form class="content__contacts contacts__form js-contact-form" method="post" action=""> -->
+                  <form class="content__contacts contacts__form" method="post" action="">
                     <div class="content__brief">
                       <!-- <div class="content__brief-title">
                         <label class="input-file">
